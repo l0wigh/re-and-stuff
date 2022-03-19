2af9c1e43c91f35d99cfe33d5fd73e35a24c955d
L0Wigh <mathiotthomas@gmail.com>
2022-03-19 16:58:17 +0100

    Fix du crackme 1,

--- challenges/crackme1.c
+++ challenges/crackme1.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv) {
 	if (argc != 2) {
 		printf("Usage: %s <code>\n", argv[0]);
 		return 1;
 	}
-	if (argv[1] == (char *)422022)
+	if (atoi(argv[1]) == 422022)
 		printf("Bien joué\n");
 	else
 		printf("Perdu\n");
